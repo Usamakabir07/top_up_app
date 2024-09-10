31a45c25c3fbb718670f1f599af5e0575a056c05
Usama Kabir <usama@thecloud.ae>
2024-09-10 12:43:32 +0400

    extra file is removed

--- lib/common/colors.dart
+++ lib/common/colors.dart
@@ -1,12 +1,7 @@
 import 'package:flutter/material.dart';
 
 class MyColors {
-  static var appLightColor = const Color(0xFFfcdeb4);
-  static var appLightLevel1 = const Color(0xFFfbc2a6);
-  static var appDarkLevel1 = const Color(0xFFf47880);
-  static var appDarkLevel2 = const Color(0xFF1F2327);
   static var appDark = const Color(0xFFec4d69);
-  static var blackColor = const Color(0xFF111111);
   static var whiteColor = const Color(0xFFFFFFFF);
   static var activeColor = const Color(0xFF162772);
   static var greenColor = Colors.green;
